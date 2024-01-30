d3a8223ca84a0cbe69da580acd2444b81b31cfe6
Suman Sur <sumansur98@gmail.com>
2024-01-30 21:05:48 +0530

    added material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,48 @@
-import { useState } from 'react';
-import './App.css';
-import PersonalInfo from './Components/PersonalInfo';
-import EducationInfo from './Components/EducationInfo';
-import ExperienceInfo from './Components/ExperienceInfo';
-import CvComponent from './Components/CvComponent';
-
-const PERSONALINFOOBJ = {
-  name : 'Suman Sur',
-  email : 'sumansur98@gmail.com',
-  phnNumber : '8017364915',
-  location : 'Kolkata'
-}
+import { useState } from "react";
+import "./App.css";
+import PersonalInfo from "./Components/PersonalInfo";
+import EducationInfo from "./Components/EducationInfo";
+import ExperienceInfo from "./Components/ExperienceInfo";
+import CvComponent from "./Components/CvComponent";
+import Button from '@mui/material/Button';
 
 function App() {
   const [personalObj, setPersonalObj] = useState({
-    name : '',
-    email : '',
-    phnNumber : '',
-    location : '' 
-  })
+    name: "",
+    email: "",
+    phnNumber: "",
+    location: "",
+  });
 
-  const handlePersonalChange = (key, value)=>{
-    const newObj = {...personalObj, [key] : value};
+  const handlePersonalChange = (key, value) => {
+    const newObj = { ...personalObj, [key]: value };
     setPersonalObj(newObj);
-  }
+  };
 
   return (
     <>
-      <div className='input-section'>
-        <div className="form-container personal-form-div">
-          <PersonalInfo values={personalObj} onInputChange={handlePersonalChange}></PersonalInfo>
+    <Button variant="outlined">Hello world</Button>
+      <div className="flex">
+        <div>
+          <div className="form-container personal-form-div">
+            <PersonalInfo
+              values={personalObj}
+              onInputChange={handlePersonalChange}
+            ></PersonalInfo>
+          </div>
+          <div className="form-container education-form-div">
+            <EducationInfo></EducationInfo>
+          </div>
+          <div className="form-container experience-form-div">
+            <ExperienceInfo></ExperienceInfo>
+          </div>
         </div>
-        <div className="form-container education-form-div">
-          <EducationInfo></EducationInfo>
+        <div>
+          <CvComponent personal={personalObj}></CvComponent>
         </div>
-        <div className="form-container experience-form-div">
-          <ExperienceInfo></ExperienceInfo>
-        </div>
-      </div>
-      <div>
-        <CvComponent 
-        personal={personalObj}>
-        </CvComponent>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
