749bc072d5b6edc550e0e2ab76888ead9c62a947
Suman Sur <sumansur98@gmail.com>
2024-02-01 21:48:03 +0530

    outer layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import PersonalInfo from "./Components/PersonalInfo";
 import EducationInfo from "./Components/EducationInfo";
 import ExperienceInfo from "./Components/ExperienceInfo";
 import CvComponent from "./Components/CvComponent";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 
 function App() {
   const [personalObj, setPersonalObj] = useState({
@@ -20,8 +22,35 @@ function App() {
   };
 
   return (
+    <Container maxWidth="xl">
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Grid
+            container
+            direction={"column"}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <PersonalInfo
+                values={personalObj}
+                onInputChange={handlePersonalChange}
+              ></PersonalInfo>
+            </Grid>
+            <Grid item>education</Grid>
+            <Grid item>work</Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6}>
+          <h1>xs=4</h1>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+
+  return (
     <>
-    <Button variant="outlined">Hello world</Button>
+      <Button variant="outlined">Hello world</Button>
       <div className="flex">
         <div>
           <div className="form-container personal-form-div">
--- src/Components/PersonalInfo.jsx
+++ src/Components/PersonalInfo.jsx
@@ -1,27 +1,63 @@
-export default function PersonalInfo({ values, onInputChange }){
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import TextField from '@mui/material/TextField';
 
-    const handleInputChange = (e)=>{
-        const key = e.target.id;
-        const val = e.target.value;
-        onInputChange(key, val);
-    }
+export default function PersonalInfo({ values, onInputChange }) {
+  const handleInputChange = (e) => {
+    const key = e.target.id;
+    const val = e.target.value;
+    onInputChange(key, val);
+  };
 
-    return (
-        <>
-            <h1>personal info</h1>
-            <form>
-                <label htmlFor="name" className="text-3xl font-bold underline ">Name</label>
-                <input type="text" name="" id="name" value={values.name} onChange={handleInputChange}/>
+  return (
+    <>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+            
+        </CardContent>
+      </Card>
+    </>
+  );
 
-                <label htmlFor="email">Email</label>
-                <input type="text" id="email" value={values.email} onChange={handleInputChange}/>
+  return (
+    <>
+      <h1>personal info</h1>
+      <form>
+        <label htmlFor="name" className="text-3xl font-bold underline ">
+          Name
+        </label>
+        <input
+          type="text"
+          name=""
+          id="name"
+          value={values.name}
+          onChange={handleInputChange}
+        />
 
-                <label htmlFor="phnNumber">Phone Number</label>
-                <input type="text" id="phnNumber" value={values.phnNumber} onChange={handleInputChange}/>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          value={values.email}
+          onChange={handleInputChange}
+        />
 
-                <label htmlFor="location">Location</label>
-                <input type="text" id="location" value={values.location} onChange={handleInputChange}/>
-            </form>
-        </>
-    )
+        <label htmlFor="phnNumber">Phone Number</label>
+        <input
+          type="text"
+          id="phnNumber"
+          value={values.phnNumber}
+          onChange={handleInputChange}
+        />
+
+        <label htmlFor="location">Location</label>
+        <input
+          type="text"
+          id="location"
+          value={values.location}
+          onChange={handleInputChange}
+        />
+      </form>
+    </>
+  );
 }
