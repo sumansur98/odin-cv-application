7470f0fefb513e21ad1d47ad3c5aeaaf8fe4699d
Suman Sur <sumansur98@gmail.com>
2024-02-05 13:05:44 +0530

    personal cv section progress

--- src/Components/CvComponent.jsx
+++ src/Components/CvComponent.jsx
@@ -1,13 +1,15 @@
 import PersonalInfoCV from "./PersonalInfoCV";
-import { Card } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 
-export default function CvComponent({personal}){
-    return (
-        <>
-            <Card>
-                <PersonalInfoCV values={personal}></PersonalInfoCV>
-            </Card>
-            
-        </>
-    )
+export default function CvComponent({ personal }) {
+  return (
+    <>
+      <Card sx={{ minWidth: 275, height: 100 + "vh" }}>
+        <CardContent>
+          <PersonalInfoCV values={personal}></PersonalInfoCV>
+        </CardContent>
+      </Card>
+    </>
+  );
 }
--- src/Components/PersonalInfoCV.jsx
+++ src/Components/PersonalInfoCV.jsx
@@ -1,7 +1,31 @@
-export default function PersonalInfoCV({values}){
-    return (
-        <>
-            {values.name} {values.phnNumber} {values.email} {values.location}
-        </>
-    )
+import Box from "@mui/system/Box";
+import spacing from "@mui/system/spacing";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import { palette } from "@mui/system";
+import CallIcon from '@mui/icons-material/Call';
+import EmailIcon from '@mui/icons-material/Email';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+
+export default function PersonalInfoCV({ values }) {
+  return (
+    <>
+      <Box component="section" sx={{ backgroundColor: "blue", color: "#FFF", height:'150px' }}>
+        <Typography variant="h5" gutterBottom align="center">
+          {values.name}
+        </Typography>
+        <Stack
+          direction="row"
+          spacing={2}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography><CallIcon /> {values.phnNumber}</Typography>
+          <Typography><EmailIcon />{values.email}</Typography>
+          <Typography><LocationOnIcon />{values.location}</Typography>
+        </Stack>
+      </Box>
+    </>
+  );
 }
